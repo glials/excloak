e0573c637004f5eb12c3ba9feee6b1fee84a564e
Daniel Lutz <git@daniellutz.com.br>
2023-03-29 08:04:14 -0300

    Project creation

---
+++ lib/excloak.ex
@@ -0,0 +1,18 @@
+defmodule Excloak do
+  @moduledoc """
+  Documentation for `Excloak`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Excloak.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/excloak_test.exs
@@ -0,0 +1,8 @@
+defmodule ExcloakTest do
+  use ExUnit.Case
+  doctest Excloak
+
+  test "greets the world" do
+    assert Excloak.hello() == :world
+  end
+end
